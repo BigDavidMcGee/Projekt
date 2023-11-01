9f021b2f45877160f684440107e75cdbda20ed83
BigDavidMcGee <david412u@outlook.com>
2023-11-01 08:49:23 +0100

    fgfg

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/product1')
+def product1():
+    return render_template('product1.html')
+
+@app.route('/product2')
+def product2():
+    return render_template('product2.html')
+
+@app.route('/product3')
+def product3():
+    return render_template('product3.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
