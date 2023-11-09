4dd68e8bf0936660bf2b05d699a78f5c03267ded
BigDavidMcGee <david412u@outlook.com>
2023-11-09 09:57:34 +0100

    JEG ER SEJ?1

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import json
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,6 +21,10 @@ def product2():
 @app.route('/product3')
 def product3():
     return render_template('product3.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
