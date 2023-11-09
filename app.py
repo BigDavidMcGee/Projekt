fd70e9e0e10051dce587583ce9e69448c56751fa
BigDavidMcGee <david412u@outlook.com>
2023-11-09 10:10:50 +0100

    naaa

--- app.py
+++ app.py
@@ -8,7 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user_logged_in = True)
+
+@app.route('/login')
+def login():
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return redirect("/")
 
 @app.route('/product1')
 def product1():
